68136f5e43c0c9fbdcc17eaebd65fb1d14bd431c
karoriyo <kerubelos20002000@yahoo.co.jp>
2012-12-20 23:06:26 +0900

    test push

---
+++ sampleTests.h
@@ -0,0 +1,13 @@
+//
+//  sampleTests.h
+//  sampleTests
+//
+//  Created by 宏明 加野 on 12/02/05.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface sampleTests : SenTestCase
+
+@end
